a72e277292a3709f3332a2ed6a4e2f0727211285
fladi06 <hl9306@naver.com>
2021-10-25 21:18:52 +0900

    알파벳 소문자, 대문자, 숫자 0-9중 하나가 주어졌을 때, 주어진 글자의 아스키 코드값을 출력하는 프로그램을 작성하시오.

---
+++ 소스.cpp
@@ -0,0 +1,17 @@
+/*
+알파벳 소문자, 대문자, 숫자 0-9중 하나가 주어졌을 때, 주어진 글자의 아스키 코드값을 출력하는 프로그램을 작성하시오.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	char A;
+	cin >> A;
+	int Result = A;
+	cout << Result;
+
+	return 0;
+}
